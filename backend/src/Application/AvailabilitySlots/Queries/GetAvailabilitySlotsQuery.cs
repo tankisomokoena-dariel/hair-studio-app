cb6f62dc47cf6a26a8f82cd6d85bcf9cd6109947
Tankiso Mokoena <tankiso.mokoena@dariel.co.za>
2023-10-19 12:10:25 +0200

    Implement CQRS :  Added query handler for querying all availability slots

---
+++ backend/src/Application/AvailabilitySlots/Queries/GetAvailabilitySlotsQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Application.AvailabilitySlots.Dto;
+using backend.Domain.Entities;
+using MediatR;
+
+namespace backend.Application.AvailabilitySlots.Queries;
+//public class GetAvailabilitySlotsQuery : IRequest<List<AvailabilitySlotDto>>
+//{
+//}
+
+public record GetAvailabilitySlotsQuery : IRequest<List<AvailabilitySlotDto>>;
