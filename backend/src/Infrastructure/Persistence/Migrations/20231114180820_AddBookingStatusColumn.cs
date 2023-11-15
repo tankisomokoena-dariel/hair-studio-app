60ce3caf3cb18a338971c134324b0623b18bb518
Tankiso Mokoena <tankiso.mokoena@dariel.co.za>
2023-11-15 23:44:08 +0200

    Added migrations for BookingStatus column. Updated BookingRepository.

---
+++ backend/src/Infrastructure/Persistence/Migrations/20231114180820_AddBookingStatusColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingStatusColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bookings");
+        }
+    }
+}
