e33a9478849693e5bccc37ff246a2882cd72cf11
Tankiso Mokoena <tankiso.mokoena@dariel.co.za>
2024-01-09 23:06:25 +0200

    Adding API

---
+++ new_backend/backend/API/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿
+namespace API
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
---
+++ new_backend/backend/API/Controllers/AvailabilitySlotsController.cs
@@ -0,0 +1,54 @@
+﻿using Application.AvailabilitySlots.DTO;
+using Application.AvailabilitySlots.Services;
+using backend.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvailabilitySlotsController : ControllerBase
+    {
+        private readonly IAvailabilitySlotService _availabilitySlotService;
+        public AvailabilitySlotsController(IAvailabilitySlotService availabilitySlotService) 
+        {
+            _availabilitySlotService = availabilitySlotService;
+        }
+        // GET: api/<AvailabilitySlotsController>
+        [HttpGet]
+        public async Task<IEnumerable<AvailabilitySlotDTO>?> GetAllAvailabilitySlots()
+        {
+            return await _availabilitySlotService.GetAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AvailabilitySlotDTO?> GetAvailabilitySlot(Guid id)
+        {
+            return await _availabilitySlotService.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAvailabilitySlot(AvailabilitySlotDTO availabilitySlot)
+        {
+            var result = await _availabilitySlotService.AddAsync(availabilitySlot);
+            return result.Succeeded ? Ok() : BadRequest(result.Errors);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAvailabilitySlot(AvailabilitySlotDTO availabilitySlot)
+        {
+            var result = await _availabilitySlotService.UpdateAsync(availabilitySlot);
+            return result.Succeeded ? Ok() : BadRequest(result.Errors);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAvailabilitySlot(Guid id)
+        {
+            var result = await _availabilitySlotService.DeleteAsync(id);
+            return result.Succeeded ? Ok() : BadRequest(result.Errors);
+        }
+    }
+}
