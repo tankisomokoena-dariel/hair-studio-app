83462cafe9d04472e35a329f32b328e3181fa52e
Tankiso Mokoena <tankiso.mokoena@dariel.co.za>
2024-01-24 22:09:39 +0200

    Remove the auto-generated backend

--- backend/src/Application/Common/Interfaces/ICsvFileBuilder.cs
+++
@@ -1,8 +0,0 @@
-﻿using backend.Application.TodoLists.Queries.ExportTodos;
-
-namespace backend.Application.Common.Interfaces;
-
-public interface ICsvFileBuilder
-{
-    byte[] BuildTodoItemsFile(IEnumerable<TodoItemRecord> records);
-}
